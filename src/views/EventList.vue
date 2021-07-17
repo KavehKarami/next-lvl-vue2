7a235f0ba38883b146e424006c022254390996f9
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-17 18:11:07 +0430

    init

---
+++ src/views/EventList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import api from "../services/EventService";
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  mounted() {
+    api
+      .getEvents()
+      .then((res) => (this.events = res.data))
+      .catch((e) => console.log(e));
+  },
+};
+</script>
